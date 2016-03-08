e1224961158d1aec4dbd1b7cfabecef19098375a
Ana Aldescu <ana_georgia.aldescu@stud.acs.upb.ro>
2016-03-08 21:04:53 +0200

    Implemented task 5

--- PhoneDialer/src/ro/pub/cs/systems/pdsd/lab03/phonedialer/PhoneDialerActivity.java
+++ PhoneDialer/src/ro/pub/cs/systems/pdsd/lab03/phonedialer/PhoneDialerActivity.java
@@ -1,17 +1,87 @@
 package ro.pub.cs.systems.pdsd.lab03.phonedialer;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
 
 
 public class PhoneDialerActivity extends Activity {
+	
+	private class ButtonClick implements View.OnClickListener {
 
+		@Override
+		public void onClick(View view) {
+			Button btn = (Button) view;
+			String btnValue = btn.getText().toString();
+			EditText phoneNumber = (EditText) findViewById(R.id.editTextNumber);
+			
+			phoneNumber.setText(phoneNumber.getText() + btnValue);
+		}
+		
+	}
+	
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_phone_dialer);
+        
+        ButtonClick btnClick = new ButtonClick();
+        
+        int[] btnIds = {R.id.button1, R.id.button2, R.id.button3, R.id.button4, R.id.button5,
+        				R.id.button6, R.id.button7, R.id.button8, R.id.button9, R.id.button10,
+        				R.id.button11, R.id.button12};
+        
+        for (int id : btnIds) {
+        	Button btn = (Button) findViewById(id);
+        	
+        	btn.setOnClickListener(btnClick);
+        }
+        
+        ImageButton backspace = (ImageButton) findViewById(R.id.imageButton1);
+        ImageButton call = (ImageButton) findViewById(R.id.imageButton2);
+        ImageButton hangup = (ImageButton) findViewById(R.id.imageButton3);
+        
+        backspace.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				EditText editText = (EditText) findViewById(R.id.editTextNumber);
+				String phoneNumber = editText.getText().toString();
+				
+				if (phoneNumber.length() > 0) {
+					editText.setText(phoneNumber.subSequence(0, phoneNumber.length() - 1));
+				}
+			}
+		});
+        
+        call.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				EditText editText = (EditText) findViewById(R.id.editTextNumber);
+				String phoneNumber = editText.toString();
+				
+				Intent intent = new Intent(Intent.ACTION_CALL);
+					
+				intent.setData(Uri.parse("tel:" + phoneNumber));
+				startActivity(intent);
+			}
+		});
+        
+        hangup.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				finish();
+			}
+		});
     }
 
 
